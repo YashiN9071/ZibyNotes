1dd032a2fae45f0e07a7c61b78e7f1fd989845e9
NickYashin <nik.yashin.03@inbox.ru>
2022-04-12 22:24:45 +0300

    v1.1

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 # t.me/ziby_notes_bot
 import logging
+
 from telegram.ext import Updater, CommandHandler, ConversationHandler
 from telegram import ReplyKeyboardMarkup
 
 # logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-TOKEN = '5143073002:AAFw3VjKfzbW75aDOmOpFxawN6QZxfP5ZX0'
+TOKEN = '5387683486:AAEHQB94zVgmg3JcYPQFmgHR_ZcmCy47SOU'
 
-# keyboard
-reply_keyboard = [['/create_note', '/create_remind'],
-                  ['/view_notes', '/view_reminds'],
+# keyboards
+reply_keyboard = [['/notes', '/reminds'],
                   ['/help', '/stop']]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+main_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+notes_reply_keyboard = [['/create_note', '/view_notes'],
+                        ['/delete_note', '/come_back']]
+notes_markup = ReplyKeyboardMarkup(notes_reply_keyboard, one_time_keyboard=True)
+
+reminds_reply_keyboard = [['/create_remind', '/view_reminds'],
+                          ['/delete_remind', '/come_back']]
+reminds_markup = ReplyKeyboardMarkup(reminds_reply_keyboard, one_time_keyboard=True)
 
 
 # functions and commands
 def main():
     updater = Updater(TOKEN)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('notes', notes))
+    dp.add_handler(CommandHandler('reminds', reminds))
     dp.add_handler(CommandHandler('help', help))
-    dp.add_handler(CommandHandler('create_note', create_note))
-    dp.add_handler(CommandHandler('create_remind', create_remind))
-    dp.add_handler(CommandHandler('view_notes', view_notes))
-    dp.add_handler(CommandHandler('view_reminds', view_reminds))
+    dp.add_handler(CommandHandler('stop', stop))
     updater.start_polling()
 
     updater.idle()
@@ -33,7 +39,7 @@ def main():
 def start(update, context):
     update.message.reply_text(
         'Давайте начнём!',
-        reply_markup=markup
+        reply_markup=main_markup
     )
 
 
@@ -43,14 +49,27 @@ def stop(update, context):
 
 
 def help(update, context):
-    update.message.reply_text("""Я имею следующие функции: 
-    /create_note
-    /create_remind 
-    /view_notes 
-    /view_remind""")
+    update.message.reply_text('У меня есть следующие функции: ')
+
+
+def notes(update, context):
+    update.message.reply_text('Здесь вы можете посмотреть все ваши существующие заметки, добавить новую\
+     или удалить старую заметку', reply_markup=notes_markup)
 
 
 def create_note(update, context):
+    update.message.reply_text('Напишите новую заметку')
+
+
+def view_notes(update, context):
+    update.message.reply_text('Вот ваши заметки')
+
+
+def delete_note(update, context):
+    update.message.reply_text('')
+
+
+def reminds(update, context):
     update.message.reply_text('Напишите новую заметку')
 
 
@@ -58,12 +77,12 @@ def create_remind(update, context):
     update.message.reply_text('Напишите новую напоминалку')
 
 
-def view_notes(update, context):
-    update.message.reply_text('Вот ваши заметки')
-
-
 def view_reminds(update, context):
     update.message.reply_text('Вот ваши напоминалки')
+
+
+def delete_remind(update, context):
+    update.message.reply_text('')
 
 
 # start program
